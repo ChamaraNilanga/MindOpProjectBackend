d6bd06a76a4b82dd0d7425708ef1f4881ef07880
Rashmika Jayathilaka <63368361+Rashmika2020@users.noreply.github.com>
2022-06-20 18:50:44 +0530

    index, package

--- MindOptibackend/index.js
+++ MindOptibackend/index.js
@@ -1,14 +1,48 @@
-const express = require("express");
+
 const bodyParser = require ("body-parser");
 const cors = require ("cors");
-const app = express(); 
 const pool = require("./db");
 require('dotenv').config();
 const courseroutes = require("./models/course");
-const chatroutes = require("./models/chat");
+// const chatroutes = require("./models/chat");
 const blogroutes = require("./models/blog");
 const path = require('path')
 const forumroutes=require("./models/forum");
+const paymentroutes=require("./models/payment")
+
+
+
+
+
+const express = require("express")
+var app = express();
+var server = app.listen(3010);
+var io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+});
+
+
+let clients = [];
+let messageList = [];
+
+io.on('connection', (socket) => {
+  clients.push(socket);
+  socket.emit('messageList', messageList);
+  socket.on('onMessage', data => {
+      messageList.push({
+          message:data.message,
+          senderId: socket.id
+      });
+      clients.map((cl)=> cl.emit('messageList', messageList))
+
+  });
+
+  socket.on('disconnect', () => {
+      clients = clients.filter((client, index) => client.id !== socket.id);
+  });
+});
 
 app.get("/",(req,res) =>{
     res.send("hello");
@@ -16,7 +50,7 @@ app.get("/",(req,res) =>{
 });
 
 
-app.use(cors())
+app.use(cors()) 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(function (req, res, next) {
@@ -29,9 +63,10 @@ app.use(function (req, res, next) {
 })
 
 app.use("/coursedetails",courseroutes);
-app.use("/message",chatroutes);
+// app.use("/message",chatroutes);
 app.use("/blog",blogroutes);
 app.use("/forums",forumroutes);
+app.use("/payment",paymentroutes);
 
 
 
