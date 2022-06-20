712f09cff8fb349f8443bf56c3632788f7541489
ChamaraNilanga <chamaranilanga1999@gmail.com>
2022-06-20 17:31:18 +0530

    modified

--- MindOptibackend/index.js
+++ MindOptibackend/index.js
@@ -28,7 +28,8 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept',
+    'Content-Type: multipart/form-data'
   )
   next()
 })
